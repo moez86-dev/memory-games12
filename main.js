8906767211265170b4b27824d0ea1b27308e4258
moez86-dev <mly475229@gmail.com>
2024-07-20 00:37:38 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,76 @@
+const cards = [
+    'A', 'A', 'B', 'B', 'C', 'C', 'D', 'D',
+    'E', 'E', 'F', 'F', 'G', 'G', 'H', 'H'
+];
+
+let firstCard = null;
+let secondCard = null;
+let lockBoard = false;
+
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+
+function createBoard() {
+    const gameBoard = document.getElementById('game-board');
+    shuffle(cards);
+    cards.forEach(card => {
+        const cardElement = document.createElement('div');
+        cardElement.classList.add('card');
+        cardElement.dataset.value = card;
+        cardElement.addEventListener('click', flipCard);
+        gameBoard.appendChild(cardElement);
+    });
+}
+
+function flipCard() {
+    if (lockBoard) return;
+    if (this === firstCard) return;
+    
+    this.classList.add('flipped');
+    this.textContent = this.dataset.value;
+
+    if (!firstCard) {
+        firstCard = this;
+        return;
+    }
+
+    secondCard = this;
+    lockBoard = true;
+
+    checkForMatch();
+}
+
+function checkForMatch() {
+    if (firstCard.dataset.value === secondCard.dataset.value) {
+        disableCards();
+        return;
+    }
+
+    unflipCards();
+}
+
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+    resetBoard();
+}
+
+function unflipCards() {
+    setTimeout(() => {
+        firstCard.classList.remove('flipped');
+        secondCard.classList.remove('flipped');
+        firstCard.textContent = '';
+        secondCard.textContent = '';
+        resetBoard();
+    }, 1500);
+}
+
+function resetBoard() {
+    [firstCard, secondCard, lockBoard] = [null, null, false];
+}
+
+createBoard();
